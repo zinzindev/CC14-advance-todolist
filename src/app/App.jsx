6948cfc43ece5f8a005b1c278261bae9ad3a020d
zinzindev <zinzin30@windowslive.com>
2024-12-17 15:29:47 +0700

    tailwind with login page

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,12 +2,18 @@ import './App.scss';
 // import Router from '../routes/Router';
 // import { useAuth } from '../hooks/useAuth';
 import TodoPage from '../pages/TodoPage';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
+import ProfilePage from '../pages/ProfilePage';
 
 function App() {
 	// let isLogin  = true;
 	// const { user, isAuth } = useAuth();
 
-	return <TodoPage />;
+	// return <TodoPage />;
+	return <LoginPage />;
+	// return <RegisterPage />;
+	// return <ProfilePage />;
 }
 
 export default App;
