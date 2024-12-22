677b25406cede48565f32e2fb21893ac6c80b693
zinzindev <zinzin30@windowslive.com>
2024-12-22 22:11:21 +0700

    spinner

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -5,6 +5,7 @@ import TodoPage from '../pages/TodoPage';
 import LoginPage from '../pages/LoginPage';
 import RegisterPage from '../pages/RegisterPage';
 import ProfilePage from '../pages/ProfilePage';
+import { Spinner } from '../components/Common/Spinner';
 
 function App() {
 	// let isLogin  = true;
@@ -13,7 +14,8 @@ function App() {
 	// return <TodoPage />;
 	// return <LoginPage />;
 	// return <RegisterPage />;
-	return <ProfilePage />;
+	// return <ProfilePage />;
+	return <Spinner />;
 }
 
 export default App;
